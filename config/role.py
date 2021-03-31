6c845f3c76f7250dfefbd5495da5fdbb8479f3b4
xzr <xzregg>
2021-03-31 12:29:30 +0800

    perf: 添加默认角色

--- config/role.py
+++ config/role.py
@@ -8,9 +8,11 @@
 
 
 from framework.translation import _
+
 from myadmin.models.role import Role
 
 RoleList = [
+
         ('root', _('超级管理员'), Role.RoleType.USER, ''),
         ('analysis', _('分析用户'), Role.RoleType.USER, ''),
         ('yunying', _('项目运营'), Role.RoleType.USER, ''),
@@ -18,7 +20,11 @@ RoleList = [
         ('jishu', _('项目技术'), Role.RoleType.USER, ''),
         ('ceshi', _('项目测试'), Role.RoleType.USER, ''),
         ('cehua', _('项目策划'), Role.RoleType.USER, ''),
-        ('tech', _('公共技术组'), Role.RoleType.GROUP, ''),
+        ('guangka', _('光卡数据'), Role.RoleType.GROUP, ''),
+        ('tech', _('公共技术组'), Role.RoleType.GROUP, 'guangka'),
+        ('bigdata', _('大数据组'), Role.RoleType.GROUP, 'tech'),
+        ('bigdata_analys', _('大数据分析组'), Role.RoleType.GROUP, 'bigdata'),
+        ('bigdata_dev', _('大数据开发组'), Role.RoleType.GROUP, 'bigdata'),
         ('sdk', _('sdk组'), Role.RoleType.GROUP, 'tech'),
         ('sdk_server', _('服务端组'), Role.RoleType.GROUP, 'sdk'),
         ('sdk_client', _('客户端组'), Role.RoleType.GROUP, 'sdk'),
--- settings.py
+++ settings.py
@@ -12,10 +12,12 @@ APPS = ['flink_platform','djmyframework']
 APPS_ROOT = os.path.join(BASE_DIR, 'apps')
 
 from djmyframework.settings import *
+TITLE = 'Flink 管理平台'
 
+INSTALLED_APPS.remove('ldap_account')
 
 FLINK_SAVEPOINT_PATH_BACKEND_ADDRESS = "http://h1:50070"
 FLINK_ZEPPELIN_API_ADDRESS = 'http://h29:18080/'
 
 PROJECT_ROOT = BASE_DIR
-TITLE = 'Flink Platform'
+
